0915ad84181b501a4df5d75260cd8c848dc38036
mouylenglim <mouyleng@instedd.org>
2015-03-04 09:01:02 +0700

    ui on location form

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -184,7 +184,6 @@ class Collection < ActiveRecord::Base
         ord: layer.ord,
       }
     end
-
     membership = user.membership_in self
     if !user.is_guest && !membership.try(:admin?)
       lms = LayerMembership.where(user_id: user.id, collection_id: self.id).all.inject({}) do |hash, lm|
