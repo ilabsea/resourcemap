cb33dcf4bce126bcdba6dea4f692b1275b00b883
thyda_eng <thyda@instedd.com>
2015-04-02 10:45:23 +0700

    possibility to log the owner of collection

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150327090504) do
+ActiveRecord::Schema.define(:version => 20150402025648) do
 
 
   create_table "activities", :force => true do |t|
