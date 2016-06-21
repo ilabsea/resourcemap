55797c07fd9114e9eea058f4d0f77316f0a1661d
juanboca <jblandini@manas.com.ar>
2016-06-21 13:19:32 +0700

    Removing deprecation warnings when running rspec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ResourceMap::Application.config.secret_token = '7fe0756c76a4de31e9f56c2853aa6a694470088427153aaf1d76694f0132d00424b927cc5d445c010899fed8993e3409fc5f687591c48dc1a3f24cbe13e31df1'
+
+ResourceMap::Application.config.secret_key_base = 'd08b2f53cf7e6a90d6a8e0066c4c314cf6eb99c295dbd75ccddb7f44758a6e71490f047da673d3367928f6dcbdca1335781fe166cd6821983d2062e2a53e5c05'
